80641eece95d93678a3e393904feda515715e912
Nabil1998 <63466426+NABIL-ADNAN1998@users.noreply.github.com>
2020-11-05 22:33:59 +0600

    Add files via upload

---
+++ codeforces_572A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n1, n2, k, m, i, j;
+    cin >> n1 >> n2 >> k>>m;
+    int a[n1], b[n2];
+    for (i = 0; i < n1; i++) {
+        cin >> a[i];
+    }
+    for (i = 0; i < n2; i++) {
+        cin >> b[i];
+    }
+    if (a[k - 1] < b[n2 - m]) {
+        cout << "YES\n";
+
+    } else {
+        cout << "NO\n";
+    }
+
+}
